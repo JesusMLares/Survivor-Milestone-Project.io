3ced879e4d24a1dc89927922db439ece2efeed3f
JesusMLares <twinlares2@hotmail.com>
2024-04-13 18:32:46 -0700

    Added reticle with rotation around player

--- js/index.js
+++ js/index.js
@@ -32,6 +32,30 @@ class Player{
     }
 }
 
+class Crosshair{
+    constructor(game){
+        this.game = game;
+        this.x = this.game.width * 0.5;
+        this.y = this.game.height * 0.5;
+        this.radius = 10;
+
+        this.image = document.getElementById('player');
+        this.aim;
+    }
+
+    draw(context){
+        context.drawImage(this.image, this.x - (this.radius - 1.5), this.y - (this.radius - 1.5));
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.stroke();
+    }
+    update(){
+        this.aim = this.game.calcAim(this.game.mouse, this.game.player)
+        this.x = this.game.player.x + 30 * this.aim [0];
+        this.y = this.game.player.y + 30 * this.aim[1];
+    }
+}
+
 class Game{
     constructor(canvas){
         //Properties of game: size, objects, etc
@@ -39,6 +63,7 @@ class Game{
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.player = new Player(this);
+        this.crosshair = new Crosshair(this);
 
         //Mouse properties
         this.mouse = {
@@ -50,26 +75,31 @@ class Game{
         this.keys = [];
 
         /*** event listeners & controls ***/
+        //Adds key to array on kydown
         window.addEventListener('keydown', e =>{
             if(this.keys.indexOf(e.key) === -1) this.keys.push(e.key); //adds keys to array once
         });
 
+        //Removes keys from array on keyup
         window.addEventListener('keyup', e =>{
             const index = this.keys.indexOf(e.key);
-            if(index > -1) this.keys.splice(index, 1)//removes keys from array on keyup
+            if(index > -1) this.keys.splice(index, 1);
  
         });
 
-        //Track mousemovement
+        //Track mousemovement of mouse
         window.addEventListener('mousemove', (e)=>{
             this.mouse.x = e.offsetX;
             this.mouse.y = e.offsetY;
-        })
+        });
     }
     //Render player, projectiles, etc
     render(context){
         this.player.draw(context);
         this.player.update()
+
+        this.crosshair.draw(context)
+        this.crosshair.update()
 
         //creats a visual line from the player to the mouse
         context.beginPath();
@@ -77,6 +107,17 @@ class Game{
         context.lineTo(this.mouse.x, this.mouse.y);
         context.stroke()
     }
+    //Calculates aim reticle from player
+    calcAim(a, b){//(mouse, player)
+        //distance between two objects 
+        const dx = a.x - b.x; 
+        const dy = a.y - b.y;
+        //JavaScript Pythagorean Theorem
+        const distance = Math.hypot(dx, dy);
+        const aimX = dx / distance;
+        const aimY = dy / distance;
+        return[aimX, aimY, dx, dy];
+    };
 }
 
 window.addEventListener('load', function(){
