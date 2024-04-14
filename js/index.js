c85e2a72aec7e473047522597e105d4fda6bb856
JesusMLares <twinlares2@hotmail.com>
2024-04-13 17:38:51 -0700

    Code Restructured! Current Features: Player movement and mouse tethering.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,20 @@
 class Player{
     constructor(game){
-        this.game = game; //pointing to game
-        /*** properties of player ***/
-        this.width = 25;
-        this.height = 25;
-        this.x = this.game.width * 0.5 - this.width * 0.5;
-        this.y = this.game.height * 0.5 - this.height * 0.5;
+        //Properties of the player
+        this.game = game;
+        this.x = this.game.width * 0.5;
+        this.y = this.game.height *0.5;
+        this.raidus = 20;
         this.speed = 1;
+
     }
-    draw(context){//specifies which canvas we want to draw on.
-        context.fillRect(this.x, this.y, this.width, this.height);
+        //Draws the player
+    draw(context){
+        context.beginPath();
+        context.arc(this.x, this.y, this.raidus, 0, Math.PI * 2);
+        context.stroke();
+
+
     }
     update(){ 
         //movement
@@ -25,122 +30,73 @@ class Player{
         if(this.y < 0) this.y = 0;
         else if (this.y > this.game.height -this.height) this.y = this.game.height - this.height;
     }
-    shoot(){
-        const projectile = this.game.getProjectile()
-        if(projectile) projectile.start(this.x + this.width * 0.5, this.y);
-    }
-
-
-}
-
-class Projectile{
-    constructor(){
-        this.width = 2;
-        this.height = 12;
-        this.x = 20;
-        this.y = 20;
-        this.speed = 10;
-        this.free = true; //Projectile availability 
-    }
-    draw(context){
-        if(!this.free){
-            context.fillRect(this.x, this.y, this.width, this.height);
-        }
-    }
-    update(game){
-        if(!this.free){
-            //bullet direction
-            this.y -= this.speed;
-            //bullet reset boundaries
-            if(this.y < 0 - this.height) this.reset();
-            if(this.x < 0 - this.width) this.reset();
-            if(this.x > game.width + game.width) this.reset()
-            console.log(game.width);
-            if(this.y > game.height + this.height) this.reset()
-            
-        }
-    }
-    start(x, y){
-        this.x = x - this.width * 0.5;
-        this.y = y;
-        this.free = false;
-    }
-    reset(){
-        this.free = true;
-    }
-}
-
-class Enemy{
-
 }
 
 class Game{
     constructor(canvas){
+        //Properties of game: size, objects, etc
         this.canvas = canvas;
-        this.width = this.canvas.width; 
+        this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.player = new Player(this);
-        this.keys = [];
 
-        this.projectilesPool = [];
-        this.numberOfProjectiles = 10;
-        this.createProjectiles();
+        //Mouse properties
+        this.mouse = {
+            x: 0,
+            y: 0
+        }
+
+        //Key Properties
+        this.keys = [];
 
         /*** event listeners & controls ***/
         window.addEventListener('keydown', e =>{
             if(this.keys.indexOf(e.key) === -1) this.keys.push(e.key); //adds keys to array once
-            console.log(this.keys)
         });
 
         window.addEventListener('keyup', e =>{
             const index = this.keys.indexOf(e.key);
             if(index > -1) this.keys.splice(index, 1)//removes keys from array on keyup
-            //fire button
-            if(e.key === '1') this.player.shoot();
+ 
         });
 
-    }
-    render(context, canvas){//visually renders object
-        this.player.draw(context);
-        this.player.update();
-        this.projectilesPool.forEach(projectile =>{
-            projectile.update(canvas)
-            projectile.draw(context)
+        //Track mousemovement
+        window.addEventListener('mousemove', (e)=>{
+            this.mouse.x = e.offsetX;
+            this.mouse.y = e.offsetY;
         })
     }
-    //create projectile object pool
-    createProjectiles(){
-        for (let i = 0; i < this.numberOfProjectiles; i++) {
-            this.projectilesPool.push(new Projectile()) 
-        }
+    //Render player, projectiles, etc
+    render(context){
+        this.player.draw(context);
+        this.player.update()
+
+        //creats a visual line from the player to the mouse
+        context.beginPath();
+        context.moveTo(this.player.x, this.player.y);
+        context.lineTo(this.mouse.x, this.mouse.y);
+        context.stroke()
     }
-    //get free projectile object from the pool
-    getProjectile(){
-        for (let i = 0; i < this.projectilesPool.length; i++) {
-            if(this.projectilesPool[i].free) return this.projectilesPool[i];
-            
-        }
-    }
-
-
-
 }
 
 window.addEventListener('load', function(){
+    //Canvas properties and methods
     const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d'); //Access to all 2 canvas properties.
-    canvas.width = 600;
+    const ctx = canvas.getContext('2d')
+    canvas.width = 800;
     canvas.height = 800;
 
+    //Allows game to reference canvas
     const game = new Game(canvas);
     
 
-    function animate(){//create animation loop
+    //Animation loop
+    function animate(){
+        //clears canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        game.render(ctx, canvas);
+        game.render(ctx);
         window.requestAnimationFrame(animate);
     }
 
     animate()
-
 })
